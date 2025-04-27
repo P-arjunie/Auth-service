79b19ff8edffbacb7ea47999ef0c831c03ae75a1
Pasindi Arjunie <154939837+P-arjunie@users.noreply.github.com>
2025-04-27 15:53:55 +0530

    admin

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         required:true,
-        enum: ['customer', 'restaurant', 'driver']
+        enum: ['customer', 'restaurant', 'driver', 'admin']
     }
 });
 
